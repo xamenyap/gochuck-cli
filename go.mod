54ece89cfe1863cf735cad31a0665df93b71610c
Ta Viet Anh <vietanhta@gmail.com>
2019-02-08 13:26:20 +0700

    Add feature to get random joke command

--- go.mod
+++ go.mod
@@ -1,1 +1,7 @@
 module github.com/xamenyap/gochuck-cli
+
+require (
+	github.com/spf13/cobra v0.0.3
+	github.com/spf13/pflag v1.0.3 // indirect
+	github.com/xamenyap/gochuck v0.1.0
+)
--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/spf13/cobra"
+	"github.com/xamenyap/gochuck"
+)
+
+func main() {
+	rootCmd := &cobra.Command{
+		Use:   "gochuck-cli",
+		Short: "Get Chuck Norris Jokes",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			f, err := gochuck.GetRandom()
+			if err != nil {
+				return err
+			}
+
+			cmd.Println(f.Value)
+			return nil
+		},
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
